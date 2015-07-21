b8734e09d9b0e1c7723fab2a3d56341a49860a35
nancyhawa <hawa.nancy@gmail.com>
2015-07-21 10:09:09 -0400

    broke longer methods up into shorter ones

--- jotto.rb
+++ jotto.rb
@@ -1,11 +1,9 @@
 require_relative 'wordlist'
 require_relative 'comp_choice_list'
 require_relative 'directions'
-#require_relative 'wordlistclass'
 require_relative 'wordclass'
 
 class JottoGame 
-	
 	def initialize
 		@play_list = (WordList.new(dictionary)).word_sort
 		@computer_list = (WordList.new(compwordlist)).word_sort
@@ -17,47 +15,97 @@ class JottoGame
 		@playernum = playernum?
 	end
 
-	def turn
-	puts "Guess a five letter word."
-	guess = Word.new(gets.chomp.downcase)
-	puts "Your guess was #{guess}"
+	def user_turn
+		get_response
+		check_validity
+		check_if_correct
+		give_response
+		next_turn
+	end
 
-	  if guess.downcase == @computer_word.downcase
-		puts "You won!  Congratulations"
+	def computer_makes_guess
+		@computer_guess = Word.new(@narrow_list.random_word)
+
+		puts "My turn."
+		puts "My guess is #{@computer_guess.upcase}."
+		computer_processes_response
+
+	end
+
+	def computer_processes_response
+		get_user_response
+		check_user_response_validity
+		check_response_five
+		process_response
+		no_words_error
+		user_turn
+	end
+
+	def no_words_error
+		if @narrow_list.length == 0
+			get_user_word
+			check_letter_count
+			check_repeat_letters
+			check_dictionary
+			check_response_errors
+		end
+	end
+
+	def next_turn
+		if @playernum == 1
+			user_turn
+		elsif @playernum == 2
+			computer_makes_guess
+		end
+	end
+
+	def get_response
+		puts "Guess a five letter word."
+		@guess = Word.new(gets.chomp.downcase)
+		puts "Your guess was #{@guess}"
+	end
+
+	def check_if_correct
+		if @guess == @computer_word.downcase
+			puts "You won!  Congratulations"
 	    rematch
-	  elsif @guess_list_hash.has_key?(guess)
-	  	puts "You already guessed that word."
-	  	puts "#{guess.upcase}: #{@guess_list_hash[guess]}"
-	  	turn
-	  elsif guess.downcase == "give up"
+	  elsif give_up?
 	    puts "That is super lame.  For real."
 	    puts "My word was #{@computer_word.upcase}."
 	    rematch
-	  elsif guess.downcase == "view"
-		@guess_list_hash.each do |key, value|
-			puts "#{key.upcase}: #{value}"
-		end
-		turn
-	  elsif guess.length != 5
-	    puts "Make sure your word has 5 letters!"
-	    turn
-	  elsif guess.repeat_letters? == true
-	    puts "Make sure your guess has no repeat letters."
-	    turn
-	  elsif @play_list.include?(guess) == false
-		puts "Sorry!  That is not a word!"
-		turn
-	  else
-		value = @computer_word.compare(guess)
-		puts "#{guess.upcase}: #{value}"
-		add_to_hash(guess, value, @guess_list_hash)
+	  elsif @guess == "view"
+			@guess_list_hash.each do |key, value|
+				puts "#{key.upcase}: #{value}"
+	  	end
+			user_turn
 	  end
-
-	if @playernum == 1
-		turn
-	elsif @playernum == 2
-		computer_turn
 	end
+
+	def give_up?
+		@guess == "give up"
+	end
+
+	def check_validity
+	  if @guess_list_hash.has_key?(@guess)
+	  	puts "You already guessed that word."
+	  	puts "#{@guess.upcase}: #{@guess_list_hash[@guess]}"
+	  	user_turn
+	  elsif @guess.length != 5
+	    puts "Make sure your word has 5 letters!"
+	    user_turn
+	  elsif @guess.repeat_letters? == true
+	    puts "Make sure your guess has no repeat letters."
+	    user_turn
+	  elsif @play_list.include?(@guess) == false
+			puts "Sorry!  That is not a word!"
+			user_turn
+		end
+	end
+
+	def give_response
+		value = @computer_word.compare(@guess)
+		puts "#{@guess.upcase}: #{value}"
+		add_to_hash(@guess, value, @guess_list_hash)
 	end
 
 	def playernum?
@@ -80,13 +128,11 @@ class JottoGame
 	end
 
 	def one_player
-
 		puts "I will choose a five letter word, and you must guess it."
 		puts "Every time you guess, I will tell you how many letters your word has 
 		in common with mine."
 
-	  turn
-
+	  user_turn
 	end
 
 	def add_to_hash(guess, value, hash)
@@ -114,35 +160,26 @@ class JottoGame
 		puts "You think you can beat me?  Highly unlikely."
 		puts "I'll give you a head start. You go first!"
 
-		turn
+		user_turn
+	end
+	
+	def get_user_response	
+		@response = Word.new(gets.chomp)
 	end
 
-	def computer_turn
-		computer_guess = Word.new(@narrow_list.random_word)
-
-		puts "My turn."
-		puts "My guess is #{computer_guess.upcase}."
-
-		computer_guess_method(computer_guess)
-		turn
-	end
-
-	#I separated out this method to deal with an error I was getting when a player guessed
-	#a word out of turn.	
-	def computer_guess_method(computer_guess)
-
-		response = gets.chomp
-
-		#I need to throw an error for a single letter response.
-		#When the player types a word instead of a response, it thows the correct error, and then 
-		#it asks the player again, but then the next guess is the player's last response.
-		#I don't know why the response becomes the list to choose from.
-		if [0, 1, 2, 3, 4, 5].include?(response.to_i) != true or response.length > 1
+	def check_user_response_validity
+		if ["0", "1", "2", "3", "4", "5"].include?(@response) != true 
+			or @response.length > 1
 			puts "That is not a valid response.  Your response must be an integer 
 			between 0 and 5."
-			puts "My guess was #{computer_guess.upcase}"
-			computer_guess_method(computer_guess)
-		elsif response.to_i == 5
+			puts "My guess was #{@computer_guess.upcase}"
+			@repeat = true
+			computer_processes_response
+		end
+	end
+
+	def check_response_five
+		if @response.to_i == 5
 			puts "Was that the word?  Answer Y or N."
 			answer = gets.chomp.downcase
 			if answer == "y"
@@ -151,45 +188,60 @@ class JottoGame
 				rematch
 			else 
 				puts "Ok.  I'm close though.  Not many options."
-				add_to_hash(computer_guess, response, @computer_guess_list)
-				@narrowlist = @narrow_list.refine(computer_guess, response.to_i)
-			end
-		else
-			add_to_hash(computer_guess, response, @computer_guess_list)
-			@narrowlist = @narrow_list.refine(computer_guess, response.to_i)
-			if @narrowlist.length == 0
-				no_words_error
 			end
 		end
 	end
 
+	def process_response	
+		add_to_hash(@computer_guess, @response, @computer_guess_list)
+		@narrow_list = @narrow_list.refine(@computer_guess, @response.to_i)
+	end
 
-	def no_words_error
+	def get_user_word
 		puts "One of us must have made a mistake."
 		puts "I don't think there are any words that match the parameters you've given me."
 		puts "What is your word?"
-		user_word = Word.new(gets.chomp.downcase)
+		@user_word = Word.new(gets.chomp.downcase)
+	end
 
-		if user_word.length != 5
-	    	puts "Your word didn't have five letters!  That's why I got confused."
-	  	elsif user_word.repeat_letters? == true
-	    	puts "Whoops!  Your word had a letter that repeated.  That why I got confused!"
-		elsif @play_list.include?(user_word) == false
+	def check_letter_count
+		if @user_word.length != 5
+	    puts "Your word didn't have five letters!  That's why I got confused."
+	    end_game
+	  end
+	end
+
+	def check_repeat_letters
+		if @user_word.repeat_letters? == true
+	   	puts "Whoops!  Your word had a letter that repeated.  That why I got confused!"
+	   	end_game
+		end
+	end		
+
+	def check_dictionary
+		if @play_list.include?(@user_word) == false
 			puts "Sorry!  That word isn't in my dictionary.  That's why I got confused!"
-		else
-			puts "You made some errors."		
-			@computer_guess_list.each do |key, value|
-				if user_word.compare(Word.new(key)) != value.to_i
-					puts "You told me that your word had #{value} letters in common with #{key.upcase}."  
-					puts "Actually, it has #{user_word.compare(key)} letters in common." 
-				end
+			end_game
+		end
+	end	
+		
+	def check_response_errors
+		puts "You made some errors."		
+		@computer_guess_list.each do |key, value|
+			if @user_word.compare(Word.new(key)) != value.to_i
+				puts "You told me that your word had #{value} letters in common 
+							with #{key.upcase}."  
+				puts "Actually, it has #{@user_word.compare(key)} letters in common." 
 			end
 		end
-
+		end_game
+	end
+		
+	def end_game
 		puts "My word was #{@computer_word.upcase}."
-
 		rematch
 	end
+
 end
 
 class WordList < Array
@@ -227,9 +279,7 @@ class WordList < Array
 		return WordList.new(@array)
 	
 	end
-
 end
-
 
 JottoGame.new
 
